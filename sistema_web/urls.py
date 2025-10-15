9cdd78a76181217bd8beb0c9dc665bbf6d5d1cac
MisaelPalomino <misaeljpalominor@gmail.com>
2025-10-14 23:23:35 -0500

    Se acabo de implementar las funciones basicas de cada tabla

--- sistema_web/urls.py
+++ sistema_web/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('/siscad/')
+
 
 urlpatterns = [
+    path('', home),
     path("admin/", admin.site.urls),
     path("siscad/", include("siscad.urls")),  # Enlaza tu aplicación aquí
 ]
